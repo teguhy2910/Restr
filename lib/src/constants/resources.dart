8c84a1bccda7ca6324b9b7499c27becdaa9570e4
Adithya Firmansyah Putra <aditputrafirmansyah@gmail.com>
2022-10-02 14:04:03 +0700

    add app_icon and splash screen

--- lib/src/constants/resources.dart
+++ lib/src/constants/resources.dart
@@ -1,5 +1,10 @@
 class Resources {
+  // JSONS
   static const _baseJsons = 'assets/jsons';
-
   static const String restaurantsJson = '$_baseJsons/restaurants.json';
+
+  //IMAGES
+  static const _baseImages = 'assets/images';
+  static const String restrIcon = '$_baseImages/restaurant.png';
+  static const String restrIconWithBg = '$_baseImages/restr-icon.png';
 }
--- lib/src/features/views/splash/splash_page.dart
+++ lib/src/features/views/splash/splash_page.dart
@@ -1,10 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:restr/src/constants/app_sizes.dart';
+import 'package:restr/src/constants/app_themes.dart';
+import 'package:restr/src/constants/resources.dart';
+import 'package:restr/src/routing/app_routes.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({Key? key}) : super(key: key);
 
   @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  initState() {
+    _navigateOtherScreen();
+    super.initState();
+  }
+
+  void _navigateOtherScreen() {
+    Future.delayed(const Duration(seconds: 3)).then(
+      (_) => context.goNamed(Routes.home.name),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: AppThemes.scaffoldColor,
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              Resources.restrIcon,
+              width: 80,
+              height: 80,
+            ),
+            Gap.h12,
+            Text(
+              'Restr',
+              style: AppThemes.headline1,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
